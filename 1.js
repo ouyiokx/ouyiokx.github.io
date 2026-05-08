6cbe3c7c3f90fcc4e5568d85e1e65918b78e3832
ouyiokx <prliu1@hotmail.com>
2026-05-08 23:43:39 +0800

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,16 @@
+var bta=document.getElementById("bta");
+bta.onclick=function(){
+	window.open('https://www.plokiujmnhy.com/zh-hans/join?channelId=ACE528334');
+}
+var btb=document.getElementById("btb");
+btb.onclick=function(){
+	window.open('https://www.brandifybox.com/upgradeapp/android_ACE528334.apk');
+}
+var btd=document.getElementById("btd");
+btd.onclick=function(){
+	window.open('https://www.plokiujmnhy.com/zh-hans/join?channelId=ACE528334');
+}
+var btc=document.getElementById("btc");
+btc.onclick=function(){
+	window.open('https://www.growthhivex.com/zh-hans/join?channelId=ACE528334');
+}
